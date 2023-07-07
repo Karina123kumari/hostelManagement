b1eac95cafc421d0781936a2437b2df546106876
karina-kumari <130815895+Karina123kumari@users.noreply.github.com>
2023-07-07 20:05:18 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,152 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Hostel Responsive Website</title>
+  
+  <!--FAVICON-->
+  <link rel="icon" type="image/png" href="logo/favicon.png">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+   
+    <!--IONICONS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+
+<body>
+<?php
+include 'partials/navbar.php';
+  ?>
+<!-- slider -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="slider/one.jpg" class="d-block w-100 slider" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="slider/three.jpg" class="d-block w-100 slider" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="slider/two.jpg" class="d-block w-100 slider" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <section class="Quick_Button">
+  </section>
+  <!-- Details about Website -->
+  <section class="about">
+    <div class="heading">
+      <!--<h1>About</h1>-->
+      <div class="clog-lg-6 text-center text- lg-start">
+          <h3>Welcome to <span>DreamRoom,<br class="d-none d-lg-block">
+            your success journey</span>&nbspstart from here</h3>
+            <p>We are giving you the ease to explore cheap and best hostel to live like home.
+       we provide a secure, well-maintained discplined environment for you!</p>
+        <a href="ABOUT_US.html" class="main-btn mt-4">why us?</a>
+      </div>
+    </div>
+  </section>
+  <section class="Benefits">
+    <div class="heading">
+      <div class="container">
+        <div class="row">
+          <div class="col-8">One of the most concerned thing ,when we go outside from our house is,security
+            The most stricted and maintained environment towards you is care like a guardian.
+          </div>
+          <div class="col-4">
+            <img src="benefit/guard.jpg" width="400px">
+          </div>
+        </div>
+        <div class="row" style="margin-top:10%;">
+          <div class="col-4">
+            <img src="benefit/s2.webp" width="400px">
+          </div>
+          <div class="col-8">This is how we give you a free peacefull space to spend time with 
+          your friends,colleagus and more.<br><b>The place where you can explore with your fellow.</b></font></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Footer -->
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4"><body>
+    <!-- Section: Social media -->
+    
+    <section class="mb-4">
+      <h5>Stay connected</h5>
+    </section>
+    <!-- Section: Social media -->
+    <section class="icons">
+      <a href="#"><i class="fa-2x fa fa-facebook-official" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa-2x fa fa-linkedin-square" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa-2x fa fa-whatsapp" aria-hidden="true"></i></a>
+    </section>
+    <!-- Section: Form -->
+    <section class="">
+      <form action="">
+        </div>
+        <!--Grid row-->
+      </form>
+    </section>
+    <!-- Section: Form -->
+
+    <!-- Section: Text -->
+    <section class="mb-4">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+        repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+        eum harum corrupti dicta, aliquam sequi voluptate quas.
+      </p>
+    </section>
+    <!-- Section: Text -->
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+  <!--<?php include 'footer.php';?>-->
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
